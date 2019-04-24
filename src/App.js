15c65ddf96013883bce094aef812c3d3ff6d66e4
Karoline Rivero Bernacki <kbernacki@student.unimelb.edu.au>
2019-04-24 19:02:31 +1000

    Modified 'pages' to 'UserAuth'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/Signin';
+import SignUp from './UserAuth/SignUp';
+import SignIn from './UserAuth/Signin';
+import ForgotPassword from './UserAuth/ForgotPassword';
+import NavBar from './Components/NavBar'
 import logo from './photos/logo.png';
 import './App.css';
 
@@ -10,17 +12,21 @@ class App extends Component {
     return (
       <Router>
         <div className="Header__Cont">
+
           <Link to="/">
             <img src={logo} className="App__Logo" alt = "Logo" />
           </Link>
-            <div className="PageSwitcher">
-              <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-              <NavLink to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-            </div>
+
+          <div className="PageSwitcher">
+            <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+            <NavLink to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+          </div>
+
         </div>
         <div className="App">
             <Route exact path="/sign-up" component={SignUp} />
             <Route path="/sign-in" component={SignIn} />
+            <Route path="/forgot-password" component={ForgotPassword}/>
         </div>
       </Router>
     );
