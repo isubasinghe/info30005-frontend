12728eb379edc0e7f74a6ef5701add575a280b59
Karoline Rivero Bernacki <kbernacki@student.unimelb.edu.au>
2019-04-27 13:10:37 +1000

    made forgot password page same design as signin and signup

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -10,49 +10,57 @@ class ForgotPWForm extends Component {
 		//Default state of these values
 		this.state = {
 			email: "",
-			password: ""
 		};
-
-		// When called, will cause handleChange function with the event e = this
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	handleChange (e) {
-		let target = e.target;
-		let value = target.type === "checkbox"?target.checked : target.value;
-		let name = target.name;
-
-		//Will update value when it changes
-		this.setState({
-			[name]: value
-		})
+	handleEmail = event => {
+		this.setState({email : event.target.value}); 
 	}
 
 	handleSubmit(e) {
+		// Prevent page from reloading
 		e.preventDefault();
 
-		console.log("The form was submitted with the following data:");
-		console.log(this.state);
+		const userEmail = {
+			name: this.state.name
+		}
+
+
+		// Forgot password
+		/*axios.post('http://foodspan.ap-southeast-1.elasticbeanstalk.com/auth/signin', userEmail)
+			.then(res => {
+				console.log(res.data);
+		})
+			.catch(err => {
+			console.log(err);
+		});*/
 	}
 
 	render() {
 		return (
-			<div className="FormCenter">
-				<div className="FormTitle">Forgot Password</div>
-
-				<form className="FormField" onSubmit={this.handleSubmit}>
-					<div className="FormField">
-						<label className="FormField__Label" htmlFor="email">Email</label>
-						<input type="email" id="email" className="FormField__Input"
-							placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange}/>
-					</div>
-
-					<div className="FormField">
-						<button className="FormField__Button mr-20">Reset Password</button>
-					</div>
-				</form>
-
+			<div className="row">
+				<div className="col-sm">
+				</div>
+				<div className="col-4 mt-5 pl-5 pr-5 bg-white">
+					<h2 className="text-center p-3 mt-5 text-white font-weight-lighter text-uppercase bg-blue">Reset password</h2>
+					<form onSubmit={this.handleSubmit}>
+						<div className="form-group pt-4">
+							<p className="text-center text-blue font-weight-lighter text-uppercase">Email</p>
+							<input type="email" className="form-control border-primary text-center text-blue font-weight-light"
+							       id="email" placeholder="Please enter you email" onChange={this.handleEmail} required />
+							<div className="invalid-tooltip"> </div>
+						</div>
+						<div className="FormField">
+							<Link to="/sign-in" className="btn text-center text-blue font-weight-light  m-4">Back to sign in</Link>
+						</div>
+						<div className="form-group text-center">
+							<button type="submit" className="btn text-center btn-white font-weight-light border-white
+							         bg-bground m-4">Reset password</button>
+						</div>
+					</form>
+				</div>
+				<div className="col-sm">
+				</div>
 			</div>
 		);
 	}
