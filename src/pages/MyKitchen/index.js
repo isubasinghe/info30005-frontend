0b97ea40d01d4f5182930016dbdfe04a72a476a2
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-05-07 13:02:53 +1000

    added layout for my kitchen

--- src/pages/MyKitchen/index.js
+++ src/pages/MyKitchen/index.js
@@ -1,53 +1,134 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { NavLink }  from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 import InventoryList from '../../components/InventoryList';
+
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import './mykitchen.scss';
+
+
+
+const sliderSettingsDesktop = {
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+};
+
+const sliderSettingsMobile = {
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+};
+
+const data = [{name: 'Salmon'}, {name: 'Chicken'}, {name: 'Vegetable'}, {name: 'Beef'}, {name: 'Brocolli'}, {name: 'Apple'}];
+
+const getSliderResponsive = (device, data) => {
+  let sliderSettings = sliderSettingsDesktop;
+  if(device==='mobile') {
+    sliderSettings = sliderSettingsMobile;
+  }
+  return (
+    <Slider {...sliderSettings} >
+      {data.map((item, index) => {
+        return (
+          <div className="card" key={index}>
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <hr />
+              <p>This is food item</p>
+            </div>
+          </div>
+        );
+      })}
+    </Slider>
+  );
+}
+
+
+const getSlider = (data) => {
+  return (
+    <Fragment>
+      <MediaQuery query="(min-width: 1224px)">
+        {getSliderResponsive('desktop', data)}
+      </MediaQuery>
+      <MediaQuery  query="(max-width: 1224px)">
+        <div className="slick-mobile">
+          {getSliderResponsive('mobile', data)}
+        </div>
+      </MediaQuery>
+    </Fragment>
+  );
+} 
+
+
+const getCarousel = (data) => {
+  return (
+    <div className="carousel-container">
+      <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="https://placekitten.com/400/200" alt="First slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="https://placekitten.com/400/200" alt="Second slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="https://placekitten.com/400/200" alt="Third slide" />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+const getJumbatron = () => {
+  return (
+    <div className="jumbotron-container">
+      <div className="jumbotron">
+        <h1 className="display-4">
+          Recipe preview
+        </h1>
+        <hr className="my-4" />
+      </div>
+    </div>
+  );
+}
 
 class MyKitchen extends Component {
 
 	render() {
 
 		return (
-			<>
-			<div className="row">
-				<div className="col-sm">
-				</div>
-
-				<div className="col-md">
-					<h1 className="card-title bg-green p-1 font-weight-light mt-5 font-weight-light text-blue text-center">
-						welcome to foodspan</h1>
-
-					<div className="card m-4 pl-5 pr-5 pt-3 pb-3 border-primary float-auto">
-						<h2 className="card-title bg-green p-1 font-weight-light text-white text-center bg-blue">
-							my kitchen</h2>
-						<div className="card-body mb-3 p-3">
-							<InventoryList/>
-						</div>
-					</div>
-
-				</div>
-
-				<div className="col-sm">
-				</div>
+      
+			<div className="container">
+          <div className="row">
+            <div className="col">
+              {getSlider(data)}
+            </div>
+          </div>
+          <div className="row bottom-row">
+            <div className="col-md-6">
+              {getCarousel()}
+            </div>
+            <div className="col-md-6">
+              {getJumbatron()}
+            </div>
+          </div>
 			</div>
-
-			<div className ="row">
-				<div className="col-sm">
-				</div>
-
-				<div className="col-4 mt-5 pl-5">
-					<div className="card-body mb-3 p-3">
-						<NavLink to="/my-recipes" type="button" className="btn text-center text-white btn-white font-weight-light border-white
-							bg-bground m-4" activeClassName="btn btn-active">generate recipes</NavLink>
-					</div>
-				</div>
-
-				<div className="col-sm">
-				</div>
-
-			</div>
-
-			</>
-		)
+		);
 	};
 }
 
