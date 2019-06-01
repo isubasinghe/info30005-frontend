ff52692466c699df0298ecef1dbf159dfd3f3188
Karoline Rivero Bernacki <kbernacki@student.unimelb.edu.au>
2019-06-01 22:53:47 +1000

    changed back to piece(s) in add item as before

--- src/pages/MyKitchen/AddItem/index.js
+++ src/pages/MyKitchen/AddItem/index.js
@@ -103,7 +103,7 @@ class AddItem extends Component {
           <div className="form-group">
             <label htmlor="item-units" className="col-form-label">units</label>
             <select className="form-control text-blue" id="item-units" onChange={this.handleUnitsChange}>
-              <option>piece</option>
+              <option>piece(s)</option>
               <option>g</option>
               <option>mL</option>
             </select>
